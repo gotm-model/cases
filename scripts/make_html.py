682d54a1cce7d23b6272bab0442772eaaf98540e
Jorn Bruggeman <jorn@bolding-bruggeman.com>
2020-11-03 13:16:46 +0000

    draw badge summarizing test results

--- scripts/make_html.py
+++ scripts/make_html.py
@@ -7,6 +7,22 @@ parser.add_argument('out')
 parser.add_argument('path', nargs='*')
 args = parser.parse_args()
 
+ntest = 0
+nfailed = 0
+
+def draw_badge(out):
+    from PIL import Image, ImageDraw, ImageFont
+    nscale = 2
+    w, h = 180, 24
+    txt = Image.new('RGB', (nscale * w, nscale * h), (200,200,200))
+    d = ImageDraw.Draw(txt)
+    d.ellipse([(0.2*h*nscale,0.2*h*nscale), (0.8*h*nscale,0.8*h*nscale)], fill=(255,0,0) if nfailed > 0 else (0,255,0), outline=(0,0,0), width=1)
+    status = 'all %i tests succeeded' % ntest if nfailed == 0 else '%i/%i tests failed' % (nfailed, ntest)
+    txt = txt.resize((w, h))
+    d = ImageDraw.Draw(txt)
+    d.text((1.1*h,6), status, fill=(0,0,0))
+    txt.save(out)
+
 def get_error(task):
     errors = []
     if task.get('error') is not None:
@@ -19,12 +35,18 @@ def get_error(task):
     return errors
 
 def write_result(f, result, task_name):
+    global ntest, nfailed
     for task in result['children']:
         if task['name'] == task_name:
             errors = get_error(task)
             success = not errors
+            ntest += 1
+            if not success:
+                nfailed += 1
             classname = 'success' if success else 'failed'
             f.write('        <td class="%s"%s>%s</td>\n' % (classname, (' title="%s"' % html.escape('\n'.join(errors))) if errors else '', 'SUCCESS' if success else 'FAILED'))
+    else:
+        f.write('        <td>&nbsp;</td>')
 
 with open(args.out, 'w') as f_html:
     f_html.write("""<!DOCTYPE html>
@@ -72,3 +94,5 @@ with open(args.out, 'w') as f_html:
     </body>
     </html>
     """)
+
+draw_badge('badge.png')
